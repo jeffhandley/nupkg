9533e171544b9a6be8c784af738c67a0e81b6f11
jeffhandley <jeff@jeffhandley.com>
2012-02-01 01:39:39 -0800

    Initial Website creation

---
+++ Website/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            var user = this.ControllerContext.RouteData.Values["user"] as string;
+            if (string.IsNullOrEmpty(user))
+            {
+                return new RedirectResult("/Content/Index");
+            }
+
+            return new RedirectResult(string.Format("https://nuget.org/profiles/{0}", user));
+        }
+
+    }
+}
---
+++ Website/Routes/SubdomainRoute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace Website.Routes
+{
+    public class SubdomainRoute : RouteBase
+    {
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            var url = httpContext.Request.Headers["HOST"];
+            var index = url.IndexOf(".");
+
+            if (index < 0)
+                return null;
+
+            var subDomain = url.Substring(0, index);
+            var routeData = new RouteData(this, new MvcRouteHandler());
+            routeData.Values.Add("controller", "User"); //Goes to the UserController class
+            routeData.Values.Add("action", "Index"); //Goes to the Index action on the UserController
+            routeData.Values.Add("user", subDomain);
+
+            return routeData;
+        }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            //Implement your formating Url formating here
+            return null;
+        }
+    }
+}
